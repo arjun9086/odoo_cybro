5620f5feaf4a8a32cee808ee9285a39bfbfd9a07
arjun9086 <146924195+arjun9086@users.noreply.github.com>
2025-04-29 16:50:31 +0530

    Add files via upload

---
+++ property/__manifest__.py
@@ -0,0 +1,11 @@
+
+{
+    'name':'Property Management',
+    'version':'1.0',
+     'description':'Property Management',
+    'data':['security/ir.model.access.csv',
+            'views/property_main.xml',
+            'views/property_menu.xml'
+    ],
+    'depends':['mail']
+}
