f44c7445bc4752d22a718a6597d8fde92e8b0de1
arjun9086 <146924195+arjun9086@users.noreply.github.com>
2025-05-05 17:32:52 +0530

    Add files via upload

--- property/models/property_rental.py
+++ property/models/property_rental.py
@@ -28,52 +28,42 @@ class PropertyRental(models.Model):
     remaining_days = fields.Char(string="Remaining days", compute='_compute_remaining_days', store=True)
     company_id = fields.Many2one('res.company')
 
-    # def invoice(self):
-    #     """Invoice creation"""
-    #     # self.env['account.move'].create([
-    #     #     {'move_type': 'out_invoice'
-    #     #      }
-    #     # ])
-    #     return
+    def invoice(self):
+        """Invoice creation"""
+        return
 
+    @api.model_create_multi
+    def create(self, vals_list):
+        """Automatically generate a reference number for rental."""
+        for vals in vals_list:
+            if vals.get('name', 'New'):
+                vals['name'] = self.env['ir.sequence'].next_by_code('property.rental')
+        return super(PropertyRental, self).create(vals_list)
 
-@api.model_create_multi
-def create(self, vals_list):
-    """Automatically generate a reference number for rental."""
-    for vals in vals_list:
-        if vals.get('name', 'New'):
-            vals['name'] = self.env['ir.sequence'].next_by_code('property.rental')
-    return super(PropertyRental, self).create(vals_list)
+    @api.depends('end_date', 'start_date')
+    def _compute_remaining_days(self):
+        """remaining date"""
+        for record in self:
+            if record.end_date and record.start_date:
+                record.remaining_days = record.end_date - record.start_date
+            else:
+                record.remaining_days = 0
 
-
-@api.depends('end_date', 'start_date')
-def _compute_remaining_days(self):
-    """remaining date"""
-    for record in self:
-        if record.end_date and record.start_date:
-            record.remaining_days = record.end_date - record.start_date
+    def confirm(self):
+        """Confirm state"""
+        if self.message_attachment_count != 0:
+            self.write({'status': 'confirm'})
         else:
-            record.remaining_days = 0
+            raise ValidationError("At least 1 file must be attached")
 
+    def closed(self):
+        """closed state"""
+        self.write({'status': 'closed'})
 
-def confirm(self):
-    """Confirm state"""
-    if self.message_attachment_count != 0:
-        self.write({'status': 'confirm'})
-    else:
-        raise ValidationError("At least 1 file must be attached")
+    def returned(self):
+        """returned state"""
+        self.write({'status': 'returned'})
 
-
-def closed(self):
-    """closed state"""
-    self.write({'status': 'closed'})
-
-
-def returned(self):
-    """returned state"""
-    self.write({'status': 'returned'})
-
-
-def expire(self):
-    """expire state"""
-    self.write({'status': 'expired'})
+    def expire(self):
+        """expire state"""
+        self.write({'status': 'expired'})
