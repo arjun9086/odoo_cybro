13298adb8399f1b2b14c1a6cd4215f1d54d6f2c1
arjun9086 <146924195+arjun9086@users.noreply.github.com>
2025-05-23 17:05:56 +0530

    Delete property directory

--- property/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import property_property
-from . import property_rental
-from . import property_facility
-from . import account_move
-from . import property_line
-from . import res_partner
